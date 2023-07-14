568f4399a38a59bfc15c26ca93ddc0ca86579452
San144th <106522211+San144th@users.noreply.github.com>
2023-07-14 08:51:19 +0300

    edit files

--- hello_app/u_test.py
+++ hello_app/u_test.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from __init__ import app
+from . import app
 
 
 class BasicTests(unittest.TestCase):
--- hello_app/views.py
+++ hello_app/views.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from datetime import datetime
-from __init__ import app
+from . import app
 
 @app.route("/")
 def home():
